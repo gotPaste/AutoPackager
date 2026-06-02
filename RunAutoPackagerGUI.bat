1cac345c35e656fe26fdc126e9cd77d24106ad73
gotPaste <book@fritscheonline.com>
2026-06-02 07:57:15 -0400

    Removes MOTW and launches GUI

--- RunAutoPackagerGUI.bat
+++ RunAutoPackagerGUI.bat
@@ -1,3 +1,11 @@
+@echo off
 
+echo Removing Mark-of-the-Web via PowerShell...
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem -Recurse -File | Unblock-File"
+
+echo Done.
+
+echo Launching AutoPackager GUI
 cd %~dp0
-powershell.exe -executionpolicy bypass -windowstyle hidden -file ".\Start-autopackagergui.ps1"
+powershell.exe -executionpolicy bypass -windowstyle hidden -file ".\AutoPackager.GUI.ps1"
